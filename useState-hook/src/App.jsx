d31ccbb9b17eaef78eb68d0b87877f6da3cb17b5
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-06-20 21:06:23 +0530

    update app.jsx file

--- useState-hook/src/App.jsx
+++ useState-hook/src/App.jsx
@@ -17,3 +17,17 @@ const App = () => {
 }
 
 export default App
+
+
+
+// cleanup function run between two rendering
+   
+    // useref m rerendering nhi hoti but usestate m htoi hai 
+    // useREf can hold the state and useState can't
+    // useref can manipulate the dom
+    // useref and usestate both can do the state management
+    // custom hooks always satrt with use and we can use customHooks inside the prebuilt hooks
+    // in functional components we see the lifecycle with the useEffect hook
+    // in lifecycle....component did mount
+    // component did update
+    // component will unmount
